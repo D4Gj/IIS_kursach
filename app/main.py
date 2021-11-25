893433ecb6319755b0492e9d19ce94a2383b1e2d
D4Gj <inzadimonax@gmail.com>
2021-11-26 01:20:42 +0400

    Changes by NULS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import io
 import random
+from matplotlib import pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+
 from flask import (
     Flask,
     request,
@@ -52,7 +55,7 @@ numeric_columns = cars.loc[
 cars.acceleration_in_sec = cars.acceleration_in_sec.str.replace("sec", "").astype(
     "float"
 )
-cars.top_speed_km_per_h.str.replace("km/h", "").astype("int")
+cars.top_speed_km_per_h = cars.top_speed_km_per_h.str.replace("km/h", "").astype("int")
 cars.range_km = cars.range_km.str.replace("km", "").astype("int")
 cars.efficiency_wh_per_hour = cars.efficiency_wh_per_hour.str.replace(
     "Wh/km", ""
@@ -84,7 +87,128 @@ def html_table():
 
 @app.route("/view", methods=("POST", "GET"))
 def html_table1():
+    print(cars.tail())
     return render_template(
         "view.html",
         tables=[info.to_html(classes="data")],
     )
+
+
+@app.route("/min_price", methods=("POST", "GET"))
+def html_min():
+    print(cars.tail())
+    name, price = min_price_find()
+    price *= 1000
+    return render_template(
+        "min_price.html",
+        name=name,
+        price=price
+    )
+
+
+@app.route("/max_speed", methods=("POST", "GET"))
+def html_max_speed():
+    print(cars.tail())
+    name, speed = max_speed_find()
+    return render_template(
+        "max_speed.html",
+        name=name,
+        speed=speed
+    )
+
+
+@app.route("/mean_range", methods=("POST", "GET"))
+def html_mean_range():
+    print(cars.tail())
+    speed = mean_range_find()
+    return render_template(
+        "mean_range.html",
+        speed=speed,
+    )
+
+
+@app.route("/clustering", methods=("POST", "GET"))
+def html_clustering():
+    print(cars.tail())
+    names, values, score = clustering()
+    print(names)
+    print(list(values))
+    clusters = pd.DataFrame(data=[values], columns=names, index=["Важность"])
+
+    return render_template(
+        "clustering.html",
+        score=round(score,5),
+        tables=[clusters.to_html(classes="data")]
+    )
+
+
+# --Поиск машины с минимальной скоростью--
+def min_price_find():
+    min_price = cars['price_in_euros'][0]
+    min_index = 0
+    for i in range(len(cars['price_in_euros'])):
+        if cars['price_in_euros'][i] < min_price:
+            min_price = cars['price_in_euros'][i]
+            min_index = i
+
+    fig, ax = plt.subplots()
+    ax.set_title('Распределение цен автомобилей')
+    plt.style.context('bmh')
+    ax.set_xlabel('Цена')
+    ax.set_ylabel('Количество машин')
+    plt.hist(cars['price_in_euros'], bins=15)
+    plt.grid('on')
+    plt.savefig('static/min.png')
+    return cars['name'][min_index], min_price
+
+
+# --Поиск машины с максимальной скоростью--
+def max_speed_find():
+    max_speed = cars['top_speed_km_per_h'][0]
+    max_index = 0
+    for i in range(len(cars['top_speed_km_per_h'])):
+        if cars['top_speed_km_per_h'][i] > max_speed:
+            max_speed = cars['top_speed_km_per_h'][i]
+            max_index = i
+
+    fig, ax = plt.subplots()
+    ax.set_title('Распределение скоростей автомобилей')
+    plt.style.context('bmh')
+    ax.set_xlabel('Скорость')
+    ax.set_ylabel('Количество машин')
+    plt.hist(cars['top_speed_km_per_h'], bins=15)
+    plt.grid('on')
+    plt.savefig('static/max_speed.png')
+    return cars['name'][max_index], max_speed
+
+
+# --Поиск средней дальности хода автомобилей--
+def mean_range_find():
+    summ = 0
+    for i in range(len(cars['range_km'])):
+        summ = summ + cars['range_km'][i]
+
+    result = summ / len(cars['range_km'])
+
+    fig, ax = plt.subplots()
+    ax.set_title('Дальность хода автомобилей')
+    plt.style.context('bmh')
+    ax.set_xlabel('Дальность')
+    ax.set_ylabel('Количество машин')
+    plt.hist(cars['range_km'], bins=15)
+    plt.grid('on')
+    plt.savefig('static/mean_range.png')
+    return round(result, 2)
+
+
+def clustering():
+    x = cars[['top_speed_km_per_h', 'range_km', "number_of_seats"]]
+    y = cars['efficiency_wh_per_hour']
+    clf = DecisionTreeClassifier()
+    clf.fit(x, y)
+    print(clf.feature_names_in_)
+    # Вывод важности
+    print(clf.feature_importances_)
+    print(clf.score(x, y))  # верность классификации
+    names = ["Максимальная скорость", "Максимальная дистация", "Количество сидений"]
+    return names, clf.feature_importances_, clf.score(x, y),

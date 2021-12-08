f836de5eb1f3d748377d7e0cacb48a506a367744
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-12-09 02:42:15 +0400

    nikita start

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import io
 import random
+import matplotlib
 from matplotlib import pyplot as plt
 from sklearn.model_selection import cross_validate, cross_val_score
 from sklearn.cluster import AgglomerativeClustering
@@ -30,140 +31,117 @@ pd.set_option("display.max_rows", 500)
 pd.set_option("display.max_columns", 500)
 pd.set_option("display.width", 1000)
 
-cars = pd.read_csv("Cheapestelectriccars-EVDatabase.csv")
-cars_view = pd.read_csv("Cheapestelectriccars-EVDatabase.csv")
-cars_view["PriceinGermany"] = cars_view["PriceinGermany"].replace(np.NAN, "no info")
-cars_view.drop(columns=["PriceinUK"], inplace=True)
-cars.columns = [
-    "name",
-    "subtitle",
-    "acceleration_in_sec",
-    "top_speed_km_per_h",
-    "range_km",
-    "efficiency_wh_per_hour",
-    "fast_charge_speed_km_per_h",
-    "drive",
-    "number_of_seats",
-    "price_in_euros",
-    "price_to_drop",
-]
-cars.drop(columns="price_to_drop", inplace=True)
-numeric_columns = cars.loc[
-                  :,
-                  [
-                      "acceleration_in_sec",
-                      "top_speed_km_per_h",
-                      "range_km",
-                      "efficiency_wh_per_hour",
-                      "fast_charge_speed_km_per_h",
-                      "number_of_seats",
-                      "price_in_euros",
-                  ],
-                  ]
+data = pd.read_csv("spotify_dataset.csv")
+data_view = pd.read_csv("spotify_dataset.csv")
+# cars_view["PriceinGermany"] = cars_view["PriceinGermany"].replace(np.NAN, "no info")
+# cars_view.drop(columns=["PriceinUK"], inplace=True)
+# data.columns = [
+#     "name",
+#     "subtitle",
+#     "acceleration_in_sec",
+#     "top_speed_km_per_h",
+#     "range_km",
+#     "efficiency_wh_per_hour",
+#     "fast_charge_speed_km_per_h",
+#     "drive",
+#     "number_of_seats",
+#     "price_in_euros",
+#     "price_to_drop",
+# ]
 
-cars.acceleration_in_sec = cars.acceleration_in_sec.str.replace("sec", "").astype(
-    "float"
-)
-cars.top_speed_km_per_h = cars.top_speed_km_per_h.str.replace("km/h", "").astype("int")
-cars.range_km = cars.range_km.str.replace("km", "").astype("int")
-cars.efficiency_wh_per_hour = cars.efficiency_wh_per_hour.str.replace(
-    "Wh/km", ""
-).astype("int")
-cars.fast_charge_speed_km_per_h = cars.fast_charge_speed_km_per_h.str.replace(
-    "km/h", ""
-)
-cars.price_in_euros = (
-    cars.price_in_euros.str.replace("€", "").str.replace(",", ".").astype("float")
-)
-cars.loc[cars.price_in_euros.isnull(), "price_in_euros"] = round(
-    cars.price_in_euros.mean(), 2
-)
-info = cars.describe(percentiles=[])
+
+data.drop(columns=["Weeks Charted", "Song ID", "Week of Highest Charting"], inplace=True)
+data_view.drop(columns=["Weeks Charted", "Song ID", "Week of Highest Charting"], inplace=True)
+
+data['Streams'] = data['Streams'].str.replace(",", "").astype("int")
+data['Artist Followers'] = data['Artist Followers'].str.replace(" ", "1").astype("int")
+
+# numeric_columns = data.loc[
+#                   :,
+#                   [
+#                       "acceleration_in_sec",
+#                       "top_speed_km_per_h",
+#                       "range_km",
+#                       "efficiency_wh_per_hour",
+#                       "fast_charge_speed_km_per_h",
+#                       "number_of_seats",
+#                       "price_in_euros",
+#                   ],
+#                   ]
+#
+# data.acceleration_in_sec = data.acceleration_in_sec.str.replace("sec", "").astype(
+#     "float"
+# )
+# data.top_speed_km_per_h = data.top_speed_km_per_h.str.replace("km/h", "").astype("int")
+# data.range_km = data.range_km.str.replace("km", "").astype("int")
+# data.efficiency_wh_per_hour = data.efficiency_wh_per_hour.str.replace(
+#     "Wh/km", ""
+# ).astype("int")
+# data.fast_charge_speed_km_per_h = data.fast_charge_speed_km_per_h.str.replace(
+#     "km/h", ""
+# )
+# data.price_in_euros = (
+#     data.price_in_euros.str.replace("€", "").str.replace(",", ".").astype("float")
+# )
+# data.loc[data.price_in_euros.isnull(), "price_in_euros"] = round(
+#     data.price_in_euros.mean(), 2
+# )
+info = data.describe(percentiles=[])
 
 app = Flask(
     __name__, static_folder=config.STATIC_FOLDER, template_folder=config.TEMPLATE_FOLDER
 )
 
+matplotlib.pyplot.switch_backend('Agg')
+
 
 @app.route("/", methods=("POST", "GET"))
 def html_table():
-    cross_validation_start()
     return render_template(
         "index.html",
-        tables=[cars_view.to_html(classes="data")],
-        titles=cars_view.columns.values,
+        tables=[data_view.to_html(classes="data")],
+        titles=data_view.columns.values,
     )
 
 
-@app.route("/view", methods=("POST", "GET"))
-def html_table1():
-    print(cars.tail())
+@app.route("/steams_count", methods=("POST", "GET"))
+def html_steams_count():
+    print(data.tail())
+    listening = steams_count_find()
+    create_pdf("Отчёт по количеству прослушиваний", f"Общее количество прослушиваний составляет {listening} раз",
+               "static/steams_count.png",
+               "steams_count")
     return render_template(
-        "view.html",
-        tables=[info.to_html(classes="data")],
+        "steams_count.html",
+        listening=listening
     )
 
 
-@app.route("/min_price", methods=("POST", "GET"))
-def html_min():
-    print(cars.tail())
-    name, price = min_price_find()
-    price *= 1000
-    create_pdf("Отчёт по самой минимальной цене", f"Самой дешёвой машиной является {name} с ценой в {price} евро",
-               "static/min.png",
-               "min_price")
+@app.route("/steams_count.pdf")
+def send_pdf_streams_count():
+    return send_file(config.STATIC_FOLDER + "/steams_count.pdf")
+
+
+@app.route("/average_followers", methods=("POST", "GET"))
+def html_average_followers():
+    average = average_followers_find()
+    create_pdf("Отчёт по среднему количству подписчиков",
+               f"Среднее количество подписчиков у исполнителей составляет {average} человек.",
+               image_path="static/average_followers.png", save_name="average_followers")
     return render_template(
-        "min_price.html",
-        name=name,
-        price=price
+        "average_followers.html",
+        average=average
     )
 
 
-@app.route("/min_price.pdf")
-def send_pdf_min():
-    return send_file(config.STATIC_FOLDER + "/min_price.pdf")
-
-
-@app.route("/max_speed", methods=("POST", "GET"))
-def html_max_speed():
-    print(cars.tail())
-    name, speed = max_speed_find()
-    create_pdf("Отчёт по максимальной скорости", f"Самой быстрой машиной является {name}, со скорость {speed}.",
-               image_path="static/max_speed.png", save_name="max_speed")
-    return render_template(
-        "max_speed.html",
-        name=name,
-        speed=speed
-    )
-
-
-@app.route("/max_speed.pdf")
+@app.route("/average_followers.pdf")
 def send_pdf_max():
-    return send_file(config.STATIC_FOLDER + "/max_speed.pdf")
-
-
-@app.route("/mean_range", methods=("POST", "GET"))
-def html_mean_range():
-    print(cars.tail())
-    speed = mean_range_find()
-    create_pdf(title="Средняя дистанция прохождения",
-               text=f"В среднем электрокары проезжают {speed} километров.",
-               image_path="static/mean_range.png",
-               save_name="mean_range")
-    return render_template(
-        "mean_range.html",
-        speed=speed,
-    )
-
-
-@app.route("/mean_range.pdf")
-def send_pdf_mean_range():
-    return send_file(config.STATIC_FOLDER + "/mean_range.pdf")
+    return send_file(config.STATIC_FOLDER + "/average_followers.pdf")
 
 
 @app.route("/classification", methods=("POST", "GET"))
 def html_clustering():
-    print(cars.tail())
+    print(data.tail())
     names, values, score = classification()
     print(names)
     print(list(values))
@@ -199,68 +177,45 @@ def cross_validation():  # Кросс-валидация качество мод
     return render_template("cross_validation.html", validation=str(cross_validation_start()))
 
 
-# --Поиск машины с минимальной скоростью--
-def min_price_find():
-    min_price = cars['price_in_euros'][0]
-    min_index = 0
-    for i in range(len(cars['price_in_euros'])):
-        if cars['price_in_euros'][i] < min_price:
-            min_price = cars['price_in_euros'][i]
-            min_index = i
+# --Поиск общего количества прослушиваний--
+def steams_count_find():
+    sum = 0
+    for i in data['Streams']:
+        sum += i
 
     fig, ax = plt.subplots()
-    ax.set_title('Распределение цен автомобилей')
+    ax.set_title('Количество прослушиваний')
     plt.style.context('bmh')
-    ax.set_xlabel('Цена')
-    ax.set_ylabel('Количество машин')
-    plt.hist(cars['price_in_euros'], bins=15)
+    ax.set_xlabel('Исполнитель')
+    ax.set_ylabel('Количество прослушиваний (10 млн)')
+    plt.plot(data['Streams'])
     plt.grid('on')
-    plt.savefig('static/min.png')
-    return cars['name'][min_index], min_price
+    plt.savefig('static/steams_count.png')
+    return sum
 
 
-# --Поиск машины с максимальной скоростью--
-def max_speed_find():
-    max_speed = cars['top_speed_km_per_h'][0]
-    max_index = 0
-    for i in range(len(cars['top_speed_km_per_h'])):
-        if cars['top_speed_km_per_h'][i] > max_speed:
-            max_speed = cars['top_speed_km_per_h'][i]
-            max_index = i
+# --Поиск среднего количества подписчиков--
+def average_followers_find():
+    sum = 0
+    res = 0
+    for i in range(len(data["Artist Followers"])):
+        sum += int(data["Artist Followers"][i])
+    res = round(sum / len(data["Artist Followers"]), 0)
 
     fig, ax = plt.subplots()
-    ax.set_title('Распределение скоростей автомобилей')
+    ax.set_title('Распределение подписчиков исполнителей')
     plt.style.context('bmh')
-    ax.set_xlabel('Скорость')
-    ax.set_ylabel('Количество машин')
-    plt.hist(cars['top_speed_km_per_h'], bins=15)
+    ax.set_xlabel('Количество подписчиков (10 млн)')
+    ax.set_ylabel('Количество исполнителей')
+    plt.hist(data["Artist Followers"], bins=15)
     plt.grid('on')
-    plt.savefig('static/max_speed.png')
-    return cars['name'][max_index], max_speed
-
-
-# --Поиск средней дальности хода автомобилей--
-def mean_range_find():
-    summ = 0
-    for i in range(len(cars['range_km'])):
-        summ = summ + cars['range_km'][i]
-
-    result = summ / len(cars['range_km'])
-
-    fig, ax = plt.subplots()
-    ax.set_title('Дальность хода автомобилей')
-    plt.style.context('bmh')
-    ax.set_xlabel('Дальность')
-    ax.set_ylabel('Количество машин')
-    plt.hist(cars['range_km'], bins=15)
-    plt.grid('on')
-    plt.savefig('static/mean_range.png')
-    return round(result, 2)
+    plt.savefig('static/average_followers.png')
+    return res
 
 
 def classification():
-    x = cars[['top_speed_km_per_h', 'range_km', "number_of_seats"]]
-    y = cars['efficiency_wh_per_hour']
+    x = data[['top_speed_km_per_h', 'range_km', "number_of_seats"]]
+    y = data['efficiency_wh_per_hour']
     clf = DecisionTreeClassifier()
     clf.fit(x, y)
     print(clf.feature_names_in_)
@@ -288,8 +243,8 @@ def create_pdf(title, text, image_path, save_name):
 
 
 def cross_validation_start():
-    x = cars[['top_speed_km_per_h', 'range_km', "number_of_seats"]]
-    y = cars['efficiency_wh_per_hour']
+    x = data[['top_speed_km_per_h', 'range_km', "number_of_seats"]]
+    y = data['efficiency_wh_per_hour']
 
     clf = load_file("static/model.pkl")
 
